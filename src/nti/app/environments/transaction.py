680dda735ec30823e09246c62dbb29ad623f656a
Alex Yang <alex.yang@nextthought.com>
2019-11-20 12:21:55 -0600

    remove debugger

--- src/nti/app/environments/transaction.py
+++ src/nti/app/environments/transaction.py
@@ -1,6 +1,6 @@
 
 def default_commit_veto(request, response):
-    xtm = response.headers.get('x-tm');import pdb;pdb.set_trace()
+    xtm = response.headers.get('x-tm')
     if xtm is not None:
         return xtm != 'commit'
     if request.method == 'GET' or request.method == 'HEAD':
