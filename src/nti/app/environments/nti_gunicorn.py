a72fb79e066537a7cebe5af16ab2d4a5fe5f7c1d
cutz <christopher.m.utz@gmail.com>
2019-12-30 14:38:55 -0600

    add our own entry point so that gevent patches in time.

---
+++ src/nti/app/environments/nti_gunicorn.py
@@ -0,0 +1,23 @@
+"""
+A hack to help us ensure that we are loading and monkey-patching
+the entire system before Pyramid loads
+"""
+
+# NOTE: We must not import *anything* before the patch
+import gevent.monkey
+gevent.monkey.patch_all()
+
+logger = __import__('logging').getLogger(__name__)
+
+import sys
+from pkg_resources import load_entry_point
+
+
+def main():
+    sys.exit(
+        load_entry_point('gunicorn', 'console_scripts', 'gunicorn')()
+    )
+
+
+if __name__ == '__main__':
+    sys.exit(main())
