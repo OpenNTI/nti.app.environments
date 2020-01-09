4bac34fc0c3793fdd0412384c00ad99294c4e2fd
Alex Yang <alex.yang@nextthought.com>
2020-01-09 11:33:07 -0600

    use custom principalrolemanager.

--- src/nti/app/environments/adapters.py
+++ src/nti/app/environments/adapters.py
@@ -1,14 +1,19 @@
 from zope import component
 from zope import interface
 
-from zope.securitypolicy.interfaces import IPrincipalRoleManager
-
 from zope.securitypolicy.principalrole import AnnotationPrincipalRoleManager
 
 from nti.app.environments.models.interfaces import IOnboardingRoot
 
+from nti.app.environments.interfaces import IOnboardingRootPrincipalRoleManager
 
-@interface.implementer(IPrincipalRoleManager)
+
+@interface.implementer(IOnboardingRootPrincipalRoleManager)
+class OnboardingRootPrincipalRoleManager(AnnotationPrincipalRoleManager):
+    pass
+
+
+@interface.implementer(IOnboardingRootPrincipalRoleManager)
 @component.adapter(IOnboardingRoot)
 def _principal_role_manager(onboarding_root):
-    return AnnotationPrincipalRoleManager(onboarding_root)
+    return OnboardingRootPrincipalRoleManager(onboarding_root)
