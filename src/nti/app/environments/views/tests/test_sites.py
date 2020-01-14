0937f218305a3071a083bf95a4ad6c1500dcfd86
Alex Yang <alex.yang@nextthought.com>
2020-01-14 15:20:31 -0600

    Make sure no identical dns_names happen when uploading, tests

--- src/nti/app/environments/views/tests/test_sites.py
+++ src/nti/app/environments/views/tests/test_sites.py
@@ -17,6 +17,7 @@ from hamcrest import is_
 
 from pyramid_mailer.interfaces import IMailer
 from pyramid_mailer import Mailer
+from pyramid import httpexceptions as hexc
 
 from zope import component
 
@@ -26,6 +27,7 @@ from nti.externalization import to_external_object
 from nti.externalization.internalization.updater import update_from_external_object
 
 from nti.app.environments.views.customers import getOrCreateCustomer
+from nti.app.environments.views.sites import SitesUploadCSVView
 from nti.app.environments.views.tests import BaseAppTest
 from nti.app.environments.views.tests import with_test_app
 from nti.app.environments.views.tests import ensure_free_txn
@@ -34,10 +36,15 @@ from nti.app.environments.models.sites import TrialLicense
 from nti.app.environments.models.sites import EnterpriseLicense
 from nti.app.environments.models.sites import PersistentSite
 from nti.app.environments.models.sites import SharedEnvironment
+from nti.app.environments.models.interfaces import ICustomer
 from nti.app.environments.models.interfaces import IEnterpriseLicense
 from nti.app.environments.models.interfaces import ITrialLicense
 from nti.app.environments.models.interfaces import ISharedEnvironment
 from nti.app.environments.models.interfaces import IDedicatedEnvironment
+
+
+def _absolute_path(filename):
+    return os.path.join(os.path.dirname(__file__), 'resources/'+filename)
 
 
 class TestSiteCreationView(BaseAppTest):
@@ -426,3 +433,181 @@ class TestRequestTrialSiteView(BaseAppTest):
             assert_that(sites, has_length(3))
             assert_that(customers, has_length(3))
             assert_that(customers['12345@gmail.com'].hubspot_contact.contact_vid, '133')
+
+
+class TestSitesUploadCSVView(BaseAppTest):
+
+    def setUp(self):
+        super(TestSitesUploadCSVView, self).setUp()
+        self.url = '/onboarding/sites/@@upload_sites'
+        self.site_info = ('sites', _absolute_path('site_info.csv'))
+
+    def _f(self, file_info, content=None):
+        return file_info if content is None else (file_info[0], file_info[1], str(content))
+
+    def _upload_file(self, file, content=None, status=200):
+        return self.testapp.post(self.url,
+                                 upload_files=(self._f(file, content), ),
+                                 status=status,
+                                 extra_environ=self._make_environ(username='admin001'))
+
+    @with_test_app()
+    def test_get_or_update_dns_names(self):
+        sites = self._root().get('sites')
+        assert_that(sites, has_length(0))
+        view = SitesUploadCSVView(sites, self.request)
+        assert_that(view._get_or_update_dns_names(), has_length(0))
+        assert_that(view._existing_site_dns_names, has_length(0))
+
+        kwargs = {'license': TrialLicense(start_date=datetime.datetime(2019, 12, 12, 0, 0, 0), end_date=datetime.datetime(2019, 12, 13, 0, 0, 0)),
+                  'environment': SharedEnvironment(name='test'),
+                  'status': 'ACTIVE',}
+        site = sites.addSite(PersistentSite(dns_names=['abc.com', 'ddd.com'], **kwargs), siteId='Sxx1')
+        assert_that(view._get_or_update_dns_names(site=site), has_length(2))
+        assert_that(view._existing_site_dns_names, has_length(2))
+
+        site = sites.addSite(PersistentSite(dns_names=['xyz.com', 'opq.com'], **kwargs), siteId='Sxx2')
+        assert_that(view._get_or_update_dns_names(site), has_length(4))
+
+        site = sites.addSite(PersistentSite(dns_names=['xyz.com'], **kwargs), siteId='Sxx3')
+        assert_that(calling(view._get_or_update_dns_names).with_args(site), raises(hexc.HTTPConflict, pattern="Existing identical dns_name in existing sites: xyz.com."))
+        assert_that(view._existing_site_dns_names, has_length(4))
+
+        del view._existing_site_dns_names
+        assert_that(calling(view._get_or_update_dns_names).with_args(), raises(hexc.HTTPConflict, pattern="Existing identical dns_name in different sites: xyz.com."))
+
+        del sites['Sxx3']
+        assert_that(view._get_or_update_dns_names(), has_length(4))
+        assert_that(view._existing_site_dns_names, has_length(4))
+
+    @with_test_app()
+    @mock.patch('nti.app.environments.views.sites.get_hubspot_client')
+    @mock.patch('nti.app.environments.models.utils.get_current_request')
+    def testSitesUploadCSVView(self, mock_request, mock_client):
+        mock_request.return_value = self.request
+
+        _client = mock.MagicMock()
+        _client.fetch_contact_by_email = lambda email: None
+        mock_client.return_value = _client
+
+        sites = self._root().get('sites')
+        assert_that(sites, has_length(0))
+        customers = self._root().get('customers')
+        assert_that(customers, has_length(0))
+
+        view = SitesUploadCSVView(sites, self.request)
+
+        # dns_names
+        assert_that(view._process_dns({'nti_site': 'abc.com', 'nti_URL': 'xyz.com'}), is_(['abc.com', 'xyz.com']))
+        assert_that(view._process_dns({'nti_site': 'abc.com', 'nti_URL': ''}), is_(['abc.com']))
+        assert_that(view._process_dns({'nti_site': '', 'nti_URL': 'xyz.com'}), is_(['xyz.com']))
+        assert_that(view._process_dns({'nti_site': '', 'nti_URL': ''}), is_([]))
+
+        # license
+        result = view._process_license({'License Type': 'trial', 'LicenseStartDate': '', 'LicenseEndDate': ''})
+        assert_that(ITrialLicense.providedBy(result), is_(True))
+        assert_that(result, has_properties({'start_date': datetime.datetime(2011, 4, 1, 5, 0, 0),
+                                            'end_date': datetime.datetime(2029, 12, 31, 6, 0, 0)}))
+
+        result = view._process_license({'License Type': 'enterprise', 'LicenseStartDate': '', 'LicenseEndDate': ''})
+        assert_that(IEnterpriseLicense.providedBy(result), is_(True))
+        assert_that(result, has_properties({'start_date': datetime.datetime(2011, 4, 1, 5, 0, 0),
+                                            'end_date': datetime.datetime(2029, 12, 31, 6, 0, 0)}))
+
+        result = view._process_license({'License Type': 'internal', 'LicenseStartDate': '', 'LicenseEndDate': ''})
+        assert_that(IEnterpriseLicense.providedBy(result), is_(True))
+        assert_that(result, has_properties({'start_date': datetime.datetime(2011, 4, 1, 5, 0, 0),
+                                            'end_date': datetime.datetime(2029, 12, 31, 6, 0, 0)}))
+
+        assert_that(calling(view._process_license).with_args({'License Type': ''}), raises(ValueError, pattern="Unknown license type: ."))
+        assert_that(calling(view._process_license).with_args({'License Type': 'xyz'}), raises(ValueError, pattern="Unknown license type: xyz."))
+
+        result = view._process_license({'License Type': 'trial', 'LicenseStartDate': '2020-01-14', 'LicenseEndDate': '2020-01-15'})
+        assert_that(ITrialLicense.providedBy(result), is_(True))
+        assert_that(result, has_properties({'start_date': datetime.datetime(2020, 1, 14, 6, 0, 0),
+                                            'end_date': datetime.datetime(2020, 1, 15, 6, 0, 0)}))
+
+        # environment
+        result = view._process_environment({'Environment': 'shared:prod'})
+        assert_that(ISharedEnvironment.providedBy(result), is_(True))
+        assert_that(result, has_properties({'name': 'prod'}))
+
+        result = view._process_environment({'Environment': 'dedicated:Sxxx2', 'Host Machine': 'hh'})
+        assert_that(IDedicatedEnvironment.providedBy(result), is_(True))
+        assert_that(result, has_properties({'pod_id': 'Sxxx2', 'host': 'hh'}))
+
+        result = view._process_environment({'Environment': 'dedicated:Sxxx2', 'Host Machine': ''})
+        assert_that(IDedicatedEnvironment.providedBy(result), is_(True))
+        assert_that(result, has_properties({'pod_id': 'Sxxx2', 'host': 'host3.4pp'}))
+
+        assert_that(calling(view._process_environment).with_args({}), raises(KeyError, pattern="Environment"))
+        assert_that(calling(view._process_environment).with_args({'Environment': 'dedicated:Sxxy'}), raises(KeyError, pattern="Host Machine"))
+        assert_that(calling(view._process_environment).with_args({'Environment': ''}), raises(ValueError, pattern="Invalid Environment."))
+        assert_that(calling(view._process_environment).with_args({'Environment': 'x:y'}), raises(ValueError, pattern="Invalid Environment."))
+        assert_that(calling(view._process_environment).with_args({'Environment': 'x'}), raises(ValueError, pattern="Invalid Environment."))
+
+        # owner
+        result = view._process_owner({'Hubspot Contact': 'test@nt.com'})
+        assert_that(ICustomer.providedBy(result))
+        assert_that(result, has_properties({'email': 'test@nt.com', 'hubspot_contact': None, 'name': None}))
+        assert_that(customers, has_length(1))
+
+        result = view._process_owner({'Hubspot Contact': 'test2@nt.com'})
+        assert_that(result, has_properties({'email': 'test2@nt.com', 'hubspot_contact': None, 'name': None}))
+        assert_that(customers, has_length(2))
+
+        result = view._process_owner({'Hubspot Contact': 'test2@nt.com'})
+        assert_that(result, has_properties({'email': 'test2@nt.com', 'hubspot_contact': None, 'name': None}))
+        assert_that(customers, has_length(2))
+
+        _client.fetch_contact_by_email = lambda email: {'canonical-vid': 123, 'email': email, 'name': 'Test OK'}
+        result = view._process_owner({'Hubspot Contact': 'test3@nt.com'})
+        assert_that(result, has_properties({'email': 'test3@nt.com', 'hubspot_contact': has_properties({'contact_vid': '123'}), 'name': 'Test OK'}))
+        assert_that(customers, has_length(3))
+
+        result = view._process_owner({'Hubspot Contact': ''})
+        assert_that(result.email, is_('tony.tarleton@nextthought.com'))
+
+        assert_that(calling(view._process_owner).with_args({'Hubspot Contact': 'test'}), raises(hexc.HTTPUnprocessableEntity, pattern="Invalid email."))
+
+        # row: creator, createdTime, parent_site, status
+        assert_that(sites, has_length(0))
+        params = { 'nti_site': 'abc.com', 'nti_URL': 'xyz.com',
+                   'License Type': 'trial', 'LicenseStartDate': '2020-01-14', 'LicenseEndDate': '2020-01-15',
+                   'Environment': 'dedicated: Sxxx1', 'Host Machine': 'xx',
+                   'Hubspot Contact': 'test@nt.com',
+                   'Status': '',
+                   'Parent Site': '',
+                   'Site Created Date': ''}
+        view._process_row(params)
+        assert_that(sites, has_length(1))
+        assert_that(sites['Sxxx1'], has_properties({'dns_names': is_(['abc.com', 'xyz.com']),
+                                                    'license': has_properties({'start_date': datetime.datetime(2020,1,14,6,0,0), 'end_date': datetime.datetime(2020,1,15,6,0,0)}),
+                                                    'environment': has_properties({'pod_id': 'Sxxx1', 'host': 'xx'}),
+                                                    'owner': has_properties({'email': 'test@nt.com'}),
+                                                    'status': 'UNKNOWN',
+                                                    'parent_site': None,
+                                                    'createdTime': 1301634000,
+                                                    'creator': None
+                                                    }))
+        assert_that(view._parent_sites_ids, has_length(2))
+        assert_that(view._parent_sites_ids, has_entries({'abc.com': 'Sxxx1', 'xyz.com': 'Sxxx1'}))
+
+        params.update({'nti_site': 'abc1.com', 'nti_URL': 'xyz1.com',
+                       'Environment': 'dedicated: Sxxx2', 'Host Machine': 'xx',
+                       'Status': 'ACTIVE',
+                       'Parent Site': 'abc.com',
+                       'Site Created Date': '2020-01-14'})
+        view._process_row(params, remoteUser="testuser")
+        assert_that(sites, has_length(2))
+        assert_that(sites['Sxxx2'], has_properties({'dns_names': is_(['abc1.com', 'xyz1.com']),
+                                                    'license': has_properties({'start_date': datetime.datetime(2020,1,14,6,0,0), 'end_date': datetime.datetime(2020,1,15,6,0,0)}),
+                                                    'environment': has_properties({'pod_id': 'Sxxx2', 'host': 'xx'}),
+                                                    'owner': has_properties({'email': 'test@nt.com'}),
+                                                    'status': 'ACTIVE',
+                                                    'parent_site': is_(sites['Sxxx1']),
+                                                    'createdTime': 1578981600,
+                                                    'creator': 'testuser'
+                                                    }))
+        assert_that(view._parent_sites_ids, has_length(2))
+        assert_that(view._parent_sites_ids, has_entries({'abc.com': 'Sxxx1', 'xyz.com': 'Sxxx1',}))
