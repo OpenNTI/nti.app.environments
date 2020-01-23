7080f759de391e1ece80598be086b127f1d950fd
Alex Yang <alex.yang@nextthought.com>
2020-01-23 16:36:12 -0600

    only authenticated user can have access.

--- src/nti/app/environments/views/dns.py
+++ src/nti/app/environments/views/dns.py
@@ -1,14 +1,30 @@
+from pyramid import httpexceptions as hexc
+
 from pyramid.view import view_config
 
 from nti.externalization.interfaces import LocatedExternalDict
 
-from nti.environments.management.dns import is_dns_name_available
+from nti.environments.management.dns import is_dns_name_available as _is_dns_name_available
 
 from nti.app.environments.models.interfaces import IOnboardingRoot
 
 from nti.app.environments.models.utils import get_sites_folder
 
 from nti.app.environments.views.base import BaseView
+from nti.app.environments.views.utils import raise_json_error
+from nti.app.environments.auth import is_admin_or_account_manager
+
+
+def is_dns_name_available(dns_name, sites_folder=None):
+    """
+    A domain is available if a) there are know sites that use the domain
+    and b) there isn't a dns reservation for it.
+    """
+    sites_folder = get_sites_folder() if sites_folder is None else sites_folder
+    for site in sites_folder.values():
+        if dns_name in site.dns_names or ():
+            return False
+    return _is_dns_name_available(dns_name)
 
 
 @view_config(renderer='rest',
@@ -17,24 +33,28 @@ from nti.app.environments.views.base import BaseView
              name="check_dns_name")
 class CheckDNSNameAvailableView(BaseView):
 
-    def _is_dns_name_available(self, dns_name):
-        """
-        A domain is available if a) there are know sites that use the domain
-        and b) there isn't a dns reservation for it.
-        """
-        sites_folder = get_sites_folder(self.context,
-                                        request=self.request)
-        for site in sites_folder.values():
-            if dns_name in site.dns_names or ():
-                return False
-
-        return is_dns_name_available(dns_name)
-
     def __call__(self):
+        # This is the quick way to only allow authenticated user to access this view.
+        # later may define a specific permission?
+        userid = self.request.authenticated_userid
+        if not userid:
+            raise hexc.HTTPForbidden()
+
         params = self.request.params
         dns_name = self._get_param('dns_name', params)
 
-        is_available = self._is_dns_name_available(dns_name)
+        # Expect all dns names should be lower case.
+        # and ends with 'nextthought.io'
+        dns_name = dns_name.lower()
+
+        # Skip checking for admin and management roles.
+        if not is_admin_or_account_manager(userid, self.request):
+            if not dns_name.endswith('nextthought.io'):
+                raise_json_error(hexc.HTTPUnprocessableEntity,
+                                 "Invalid dns name.")
+
+        sites_folder = get_sites_folder(self.context, self.request)
+        is_available = is_dns_name_available(dns_name, sites_folder)
 
         result = LocatedExternalDict()
         result.__parent__ = self.context.__parent__
--- src/nti/app/environments/views/tests/test_dns.py
+++ src/nti/app/environments/views/tests/test_dns.py
@@ -16,34 +16,51 @@ from nti.app.environments.views.tests import with_test_app
 class TestCheckDNSNameAvailableView(BaseAppTest):
 
     @with_test_app()
-    @mock.patch('nti.app.environments.views.dns.is_dns_name_available')
-    def test_session_ping(self, mock_available):
+    @mock.patch('nti.app.environments.views.dns._is_dns_name_available')
+    @mock.patch('nti.app.environments.views.dns.is_admin_or_account_manager')
+    def test_dns(self, mock_admin, mock_available):
         url = '/onboarding/@@check_dns_name'
         params = {}
-        result = self.testapp.get(url, params=params, status=400, extra_environ=self._make_environ(username=None)).json_body
+        self.testapp.get(url, params=params, status=302, extra_environ=self._make_environ(username=None))
+
+        result = self.testapp.get(url, params=params, status=400, extra_environ=self._make_environ(username='user001')).json_body
         assert_that(result['message'], is_('Missing required dns_name'))
+
+        mock_available.return_value = True
+        mock_admin.return_value = True
+
+        params = {'dns_name': 'test.com'}
+        result = self.testapp.get(url, params=params, status=200, extra_environ=self._make_environ(username='user001')).json_body
+        assert_that(result, has_entries({'dns_name': 'test.com',
+                                         'is_available': True}))
+
+        mock_available.return_value = True
+        mock_admin.return_value = False
+        params = {'dns_name': 'test.com'}
+        result = self.testapp.get(url, params=params, status=422, extra_environ=self._make_environ(username='user001')).json_body
+        assert_that(result, has_entries({'message': 'Invalid dns name.'}))
 
         mock_available.return_value = False
 
-        params = {'dns_name': 'test.com'}
-        result = self.testapp.get(url, params=params, status=200, extra_environ=self._make_environ(username=None)).json_body
-        assert_that(result, has_entries({'dns_name': 'test.com',
+        params = {'dns_name': 'x.nextthought.io'}
+        result = self.testapp.get(url, params=params, status=200, extra_environ=self._make_environ(username='user001')).json_body
+        assert_that(result, has_entries({'dns_name': 'x.nextthought.io',
                                          'is_available': False}))
 
         mock_available.return_value = True
 
-        params = {'dns_name': 'test.com'}
-        result = self.testapp.get(url, params=params, status=200, extra_environ=self._make_environ(username=None)).json_body
-        assert_that(result, has_entries({'dns_name': 'test.com',
+        params = {'dns_name': 'x.nextthought.io'}
+        result = self.testapp.get(url, params=params, status=200, extra_environ=self._make_environ(username='user001')).json_body
+        assert_that(result, has_entries({'dns_name': 'x.nextthought.io',
                                          'is_available': True}))
 
         with ensure_free_txn():
             sites = self._root().get('sites')
-            sites.addSite(PersistentSite(dns_names=['test.com'],
+            sites.addSite(PersistentSite(dns_names=['x.nextthought.io'],
                                          license=TrialLicense(start_date=datetime.datetime(2015,1,1),
                                                               end_date=datetime.datetime(2016,1,1))))
 
-        params = {'dns_name': 'test.com'}
-        result = self.testapp.get(url, params=params, status=200, extra_environ=self._make_environ(username=None)).json_body
-        assert_that(result, has_entries({'dns_name': 'test.com',
+        params = {'dns_name': 'x.nextthought.io'}
+        result = self.testapp.get(url, params=params, status=200, extra_environ=self._make_environ(username='user001')).json_body
+        assert_that(result, has_entries({'dns_name': 'x.nextthought.io',
                                          'is_available': False}))
