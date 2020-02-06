5d70aa6e72854c6908af14300663eb30afb25881
Josh Zuech <josh.zuech@nextthought.com>
2020-02-06 16:09:06 -0600

    Safety if stats is not configured

--- src/nti/app/environments/stats.py
+++ src/nti/app/environments/stats.py
@@ -8,9 +8,11 @@ from nti.environments.management.interfaces import ISettings
 
 def includeme(unused_config):
     settings = component.getUtility(ISettings)
-    if 'statsd' in settings:
-        settings = settings['statsd']
-    statsd_uri = settings['statsd_uri']
-    if statsd_uri:
-        # Set up a default statsd client for others to use
-        set_statsd_client(statsd_client_from_uri(statsd_uri))
+    try:
+        statsd_uri = settings['statsd']['statsd_uri']
+    except KeyError:
+        return
+    else:
+        if statsd_uri:
+            # Set up a default statsd client for others to use
+            set_statsd_client(statsd_client_from_uri(statsd_uri))
