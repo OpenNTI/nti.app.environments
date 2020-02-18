6702fe63f048150d592276a4070c4ae4d0f703d2
Alex Yang <alex.yang@nextthought.com>
2020-02-18 14:40:58 -0600

    don't pass unchanged fields.

--- src/nti/app/environments/static/js/hosts.js
+++ src/nti/app/environments/static/js/hosts.js
@@ -37,19 +37,47 @@ function openEditingModal(me, url) {
     modal.style.display = "block";
     modal.getElementsByClassName('btnSave')[0].setAttribute('edit_url', url);
 
-    var tr = $(me).closest('tr')[0];
-    document.getElementById('host_name_edit').value = $($(tr).find("td.host")[0]).text();
-    document.getElementById('host_capacity_edit').value = $($(tr).find("td.capacity")[0]).text();
+    var tr = $(me).closest('tr')[0],
+        name_elem = $('#host_name_edit'),
+        capacity_elem = $('#host_capacity_edit');
+
+    name_elem.val($($(tr).find("td.host")[0]).text());
+    name_elem.attr('original_value', name_elem.val());
+
+    capacity_elem.val($($(tr).find("td.capacity")[0]).text());
+    capacity_elem.attr('original_value', capacity_elem.val());
+}
+
+function _is_field_changed(field_selector) {
+    var elem = $(field_selector);
+    var original = $(elem).attr('original_value');
+        incoming = $(elem).val().trim();
+
+    original = original ? original : null;
+    incoming = incoming ? incoming : null;
+    return (original === incoming) ? null : incoming
 }
 
 function editItem(me) {
-    var url = $(me).attr('edit_url');
-    var host_name = getValue("host_name_edit"),
-        capacity = getValue("host_capacity_edit");
-    var data = {
-        "host_name": host_name,
-        "capacity": capacity
-    };
+    var url = $(me).attr('edit_url'),
+        data = {},
+        updated = false,
+        fields = [['host_name', '#host_name_edit'],
+                  ['capacity', '#host_capacity_edit']];
+
+    fields.forEach(function(field){
+        var tmp = _is_field_changed(field[1]);
+        if(tmp) {
+            data[field[0]] = tmp;
+            if(!updated){
+                updated = true
+            }
+        }
+    });
+    if (!updated) {
+        showSuccessMessage('No changes.', '.success-update', '.error-update', 1000);
+        return
+    }
     data = JSON.stringify(data);
     doUpdateRequest(me, url, data, '#editingModal');
 }
--- src/nti/app/environments/static/js/site_detail.js
+++ src/nti/app/environments/static/js/site_detail.js
@@ -18,6 +18,16 @@ function doUpdate(url, data, success, error, method) {
 }
 
 // environment.
+function getSharedSelectors() {
+    return [['.name_group_item', '.site_environment_name']];
+}
+
+function getDedicatedSelectors() {
+    return [['.pod_group_item', '.site_environment_pod_id'],
+            ['.load_group_item', '.site_environment_load_factor'],
+            ['.host_group_item', '.site_environment_host']];
+}
+
 function showEnvironmentEditView() {
     var view = $('#view_environment');
     view.css('display', 'none');
@@ -25,52 +35,40 @@ function showEnvironmentEditView() {
     var edit = $('#edit_environment');
     edit.css('display', 'block');
 
-    var _type = $(view.find('.site_environment_type')[0]).text();
+    var _type = $(view.find('.site_environment_type')).text(),
+        shared = getSharedSelectors(),
+        dedicated = getDedicatedSelectors();
 
-    $(edit.find('.site_environment_type')[0]).val(_type);
+    $(edit.find('.site_environment_type')).val(_type);
 
     if (_type==='shared') {
-        $($(edit).find('.name_group_item')[0]).css('display', 'block');
-        $($(edit).find('.pod_group_item')[0]).css('display', 'none');
-        $($(edit).find('.load_group_item')[0]).css('display', 'none');
-        $($(edit).find('.host_group_item')[0]).css('display', 'none');
-
-        var elem = $(edit).find('.site_environment_name')[0];
-        $(elem).css('display', 'inline-block');
-        $(elem).val($(view.find('.site_environment_name')[0]).text());
-
-        var elem = $(edit).find('.site_environment_pod_id')[0];
-        $(elem).css('display', 'none');
-        $(elem).val('');
-
-        var elem = $(edit).find('.site_environment_load_factor')[0];
-        $(elem).css('display', 'none');
-        $(elem).val('');
-
-        var elem = $(edit).find('.site_environment_host')[0];
-        $(elem).css('display', 'none');
-        $(elem).val('');
+        shared.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'block');
+            var original = $(view.find(selector[1])).attr('original_value');
+            var elem = $(edit).find(selector[1]);
+            $(elem).css('display', 'inline-block');
+            $(elem).val( original ? original : '' );
+        });
+        dedicated.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'none');
+            var elem = $(edit).find(selector[1]);
+            $(elem).css('display', 'none');
+            $(elem).val('');
+        });
     } else {
-        $($(edit).find('.name_group_item')[0]).css('display', 'none');
-        $($(edit).find('.pod_group_item')[0]).css('display', 'block');
-        $($(edit).find('.load_group_item')[0]).css('display', 'block');
-        $($(edit).find('.host_group_item')[0]).css('display', 'block');
-
-        var elem = $(edit).find('.site_environment_pod_id')[0];
-        $(elem).css('display', 'inline-block');
-        $(elem).val($(view.find('.site_environment_pod_id')[0]).text());
-
-        var elem = $(edit).find('.site_environment_load_factor')[0];
-        $(elem).css('display', 'inline-block');
-        $(elem).val($(view.find('.site_environment_load_factor')[0]).text());
-
-        var elem = $(edit).find('.site_environment_host')[0];
-        $(elem).css('display', 'inline-block');
-        $(elem).val($(view.find('.site_environment_host')[0]).attr("host_id"));
-
-        var elem = $(edit).find('.site_environment_name')[0];
-        $(elem).css('display', 'none');
-        $(elem).val('');
+        shared.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'none');
+            var elem = $(edit).find(selector[1]);
+            $(elem).css('display', 'none');
+            $(elem).val('');
+        });
+        dedicated.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'block');
+            var original = $(view.find(selector[1])).attr('original_value');
+            var elem = $(edit).find(selector[1]);
+            $(elem).css('display', 'inline-block');
+            $(elem).val( original ? original : '' );
+        });
     }
 }
 
@@ -82,45 +80,65 @@ function cancelEnvironmentEditView() {
 
 function onEnvironmentChange () {
     var edit = $('#edit_environment');
-    var value = $(edit.find('.site_environment_type')[0]).val();
-    if (value === "shared") {
-        $($(edit).find('.name_group_item')[0]).css('display', 'block');
-        $($(edit).find('.pod_group_item')[0]).css('display', 'none');
-        $($(edit).find('.host_group_item')[0]).css('display', 'none');
+    var _type = $(edit.find('.site_environment_type')).val(),
+        shared = getSharedSelectors(),
+        dedicated = getDedicatedSelectors();
 
-        $(edit.find('.site_environment_name')[0]).css('display', 'inline-block');
-        $(edit.find('.site_environment_pod_id')[0]).css('display', 'none');
-        $(edit.find('.site_environment_load_factor')[0]).css('display', 'none');
-        $(edit.find('.site_environment_host')[0]).css('display', 'none');
-    } else if (value === "dedicated") {
-        $($(edit).find('.name_group_item')[0]).css('display', 'none');
-        $($(edit).find('.pod_group_item')[0]).css('display', 'block');
-        $($(edit).find('.load_group_item')[0]).css('display', 'block');
-        $($(edit).find('.host_group_item')[0]).css('display', 'block');
-
-        $(edit.find('.site_environment_pod_id')[0]).css('display', 'inline-block');
-        $(edit.find('.site_environment_host')[0]).css('display', 'inline-block');
-        $(edit.find('.site_environment_load_factor')[0]).css('display', 'inline-block');
-        $(edit.find('.site_environment_name')[0]).css('display', 'none');
+    if (_type === "shared") {
+        shared.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'block');
+            $(edit.find(selector[1])).css('display', 'inline-block');
+        });
+        dedicated.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'none');
+            $(edit.find(selector[1])).css('display', 'none');
+        });
+    } else if (_type === "dedicated") {
+        shared.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'none');
+            $(edit.find(selector[1])).css('display', 'none');
+        });
+        dedicated.forEach(function(selector){
+            $(edit.find(selector[0])).css('display', 'block');
+            $(edit.find(selector[1])).css('display', 'inline-block');
+        });
     }
 }
 
-function saveEnvironmentView(me, url)  {
-    var form = $(me).closest('.form-group')[0];
-    var _type = $($(form).find('.site_environment_type')[0]).val();
-    var data = {'MimeType': getEnvMimeType(_type)};
+function getEnvDataByView(view_selector, original) {
+    var view = $(view_selector);
+    var _type = original ? $(view.find('.site_environment_type')).attr('original_value'): $(view.find('.site_environment_type')).val();
     if(!_type) {
-        return;
-    }
-    if(_type==='shared') {
-        data['name'] = $($(form).find('.site_environment_name')[0]).val().trim();
-    } else {
-        data['pod_id'] = $($(form).find('.site_environment_pod_id')[0]).val().trim();
-        data['load_factor'] = $($(form).find('.site_environment_load_factor')[0]).val().trim();
-        data['host'] = $($(form).find('.site_environment_host')[0]).val().trim();
+        return null
     }
 
-    data = JSON.stringify(data);
+    var data = {'MimeType': getEnvMimeType(_type)};
+    if(_type === 'shared') {
+        var tmp = original ? $(view.find('.site_environment_name')).attr('original_value') : $(view.find('.site_environment_name')).val().trim();
+        data['name'] = tmp ? tmp : null;
+    } else {
+        var names = ['pod_id', 'load_factor', 'host'];
+        var klasses = ['.site_environment_pod_id', '.site_environment_load_factor', '.site_environment_host'];
+        names.forEach(function(field, index){
+            var tmp = original ? $(view.find(klasses[index])).attr('original_value'): $(view.find(klasses[index])).val().trim();
+            data[field] = tmp ? tmp : null;
+        });
+    }
+    return JSON.stringify(data)
+}
+
+function saveEnvironmentView(me, url)  {
+    var original = getEnvDataByView('#view_environment', true);
+    var incoming = getEnvDataByView('#edit_environment', false);
+    if (incoming === null) {
+        return
+    }
+
+    var data = (incoming === original ? null : incoming);
+    if (!data) {
+        showSuccessMessage('No changes.', '.success-edit-environment', '.error-edit-environment', 1000);
+        return
+    }
     doUpdate(url, data, '.success-edit-environment', '.error-edit-environment')
 }
 
@@ -148,20 +166,36 @@ function cancelLicenseEditView() {
     clearMessages('.success-edit-license', '.error-edit-license');
 }
 
-function saveLicenseView(me, url) {
-    var form = $(me).closest('.form-group')[0];
-    var _type = $($(form).find('.site_license_type')[0]).val();
-    var start_date = $($(form).find('.site_license_start_date')[0]).val();
-    var end_date = $($(form).find('.site_license_end_date')[0]).val();
+function getLicenseDataByView(view_selector, target_method) {
+    var view = $(view_selector);
+    var _type = $(view.find('.site_license_type'))[target_method]();
     if(!_type) {
-        return;
+        return null
     }
-    var data = {
-        'MimeType': getLicenseMimeType(_type),
-        'start_date': start_date,
-        'end_date': end_date
-    };
-    data = JSON.stringify(data);
+
+    var data = {'MimeType': getLicenseMimeType(_type)},
+        names = ['start_date', 'end_date'],
+        klasses = ['.site_license_start_date', '.site_license_end_date'];
+
+    names.forEach(function(field, index){
+        var tmp = $(view.find(klasses[index]))[target_method]().trim();
+        data[field] = tmp ? tmp : null;
+    });
+    return JSON.stringify(data)
+}
+
+function saveLicenseView(me, url) {
+    var original = getLicenseDataByView('#view_license', 'text');
+    var incoming = getLicenseDataByView('#edit_license', 'val');
+    if (incoming === null) {
+        return
+    }
+
+    var data = (incoming === original ? null : incoming);
+    if (!data) {
+        showSuccessMessage('No changes.', '.success-edit-license', '.error-edit-license', 1000);
+        return
+    }
     doUpdate(url, data, '.success-edit-license', '.error-edit-license')
 }
 
@@ -187,15 +221,36 @@ function cancelSiteEditView() {
     clearMessages('.success-edit-site-info', '.error-edit-site-info');
 }
 
+function getSiteEditData() {
+    var view = $('#view_site_info');
+    var status = $(view.find('.site_status')).text();
+    var dns_names = $(view.find('.site_dns_names')).attr('site_dns_names');
+    dns_names = dns_names ? dns_names : '';
+
+    var edit = $('#edit_site_info');
+    var new_status = $(edit.find('.site_status')).val();
+    var new_dns_names = $(edit.find('.site_dns_names')).val().trim();
+
+    var data = {},
+        updated = false;
+    if(status !== new_status) {
+        data['status'] = new_status;
+        updated = true;
+    }
+    if(dns_names !== new_dns_names) {
+        new_dns_names = new_dns_names ? $.map(new_dns_names.split(","), $.trim) : null;
+        data['dns_names'] = new_dns_names;
+        updated = true;
+    }
+    return updated ? data : null
+}
+
 function saveSiteEditView(me, url) {
-    var form = $(me).closest('.form-group')[0];
-    var status = $($(form).find('.site_status')[0]).val();
-    var dns_names = $($(form).find('.site_dns_names')[0]).val().trim();
-    dns_names = dns_names ? $.map(dns_names.split(","), $.trim) : null;
-    var data = {
-        'status': status,
-        'dns_names': dns_names
-    };
+    var data = getSiteEditData();
+    if (!data) {
+        showSuccessMessage('No changes.', '.success-edit-site-info', '.error-edit-site-info', 1000);
+        return
+    }
     data = JSON.stringify(data);
     doUpdate(url, data, '.success-edit-site-info', '.error-edit-site-info')
 }
@@ -218,12 +273,21 @@ function cancelOwnerEditView() {
     clearMessages('.success-edit-owner-info', '.error-edit-owner-info');
 }
 
+function getOwnerData() {
+    var view = $('#view_owner_info');
+    var email = $(view.find('.site_owner_email')).text().trim();
+
+    var edit = $('#edit_owner_info');
+    var new_email = $(edit.find('.site_owner_email')).val().trim();
+    return email === new_email ? null : {'owner': new_email ? new_email : null}
+}
+
 function saveOwnerEditView(me, url) {
-    var form = $(me).closest('.form-group')[0];
-    var email = $($(form).find('.site_owner_email')[0]).val();
-    var data = {
-        'owner': email,
-    };
+    var data = getOwnerData();
+    if (!data) {
+        showSuccessMessage('No changes.', '.success-edit-owner-info', '.error-edit-owner-info', 1000);
+        return
+    }
     data = JSON.stringify(data);
     doUpdate(url, data, '.success-edit-owner-info', '.error-edit-owner-info')
 }
@@ -247,12 +311,21 @@ function cancelParentSiteEditView() {
     clearMessages('.success-edit-parent-site', '.error-edit-parent-site');
 }
 
+function getParentSiteData() {
+    var view = $('#view_parent_site');
+    var parent = $(view.find('.site_parent_id')).text().trim();
+
+    var edit = $('#edit_parent_site');
+    var new_parent = $(edit.find('.site_parent_id')).val().trim();
+    return parent === new_parent ? null : {'parent_site': new_parent ? new_parent : null}
+}
+
 function saveParentSiteView(me, url) {
-    var form = $(me).closest('.form-group')[0];
-    var parent_site = $($(form).find('.site_parent_id')[0]).val();
-    var data = {
-        'parent_site': parent_site ? parent_site : null,
-    };
+    var data = getParentSiteData();
+    if (!data) {
+        showSuccessMessage('No changes.', '.success-edit-parent-site', '.error-edit-parent-site', 1000);
+        return
+    }
     data = JSON.stringify(data);
     doUpdate(url, data, '.success-edit-parent-site', '.error-edit-parent-site')
 }
